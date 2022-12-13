1b869800318ee4128b6e356efc71f5696e1c9167
a.gavrikov <aaallleeexxx918@gmail.com>
2022-12-14 00:12:27 +0300

    Поправлена верстка и все размещено по папкам

--- Assets/Scripts/IReadOnlySubscriptionProperty.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-public interface IReadOnlySubscriptionProperty<T>
-{ 
-    T Value { get; }
-    void SubscribeOnChange(Action<T> subscriptionAction);
-    void UnSubscriptionOnChange(Action<T> unsubscriptionAction);
-} 
